9ca9bafefe1eb1dd12b3d1574adb7c95015e5dc5
Huang Hai Peng <47938030+andersonhwang@users.noreply.github.com>
2024-08-03 12:26:18 +0800

    Backup 20240803

---
+++ .NET/eStationDemo/Model/ModelBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eStationDemo.Model
+{
+    /// <summary>
+    /// Model base
+    /// </summary>
+    public class ModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        /// <summary>
+        /// Notify property changed
+        /// </summary>
+        /// <param name="propertyName"></param>
+        public void NotifyPropertyChanged(string propertyName)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
---
+++ .NET/eStationDemo/ViewModel/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eStationDemo.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public string Greeting => "Hello World!";
+    }
+}

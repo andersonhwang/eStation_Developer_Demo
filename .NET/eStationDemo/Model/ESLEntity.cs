fde28e2d263bd96d7ca2f913d3d087272678d193
Huang Hai Peng <47938030+andersonhwang@users.noreply.github.com>
2024-08-05 21:54:49 +0800

    Backup 20240805

--- .NET/eStationDemo/Model/ESLEntity.cs
+++ .NET/eStationDemo/Model/ESLEntity.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace eStationDemo.Model
+﻿namespace eStationDemo.Model
 {
     internal class ESLEntity
     {
